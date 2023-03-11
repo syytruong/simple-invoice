986db0c79090d9b0048ec965db7b66a4272fb6f8
Sy Truong <sy.truong@trackinno.com>
2023-03-11 21:55:40 +0200

    add test cases for fetchOrgToken

--- src/services/authService.test.tsx
+++ src/services/authService.test.tsx
@@ -1,11 +1,16 @@
 import { API } from '../constants';
 import axios from 'axios';
 import MockAdapter from 'axios-mock-adapter';
-import { fetchAccessToken } from './authService';
+import { fetchAccessToken, fetchOrgToken } from './authService';
 
 jest.mock('axios');
 
 describe('fetchAccessToken', () => {
+  // const testUsername = 'dung+octopus4@101digital.io';
+  // const testPassword = 'Abc@123456';
+  const testUsername = 'testuser';
+  const testPassword = 'testpass';
+
   it('fetches access token successfully', async () => {
     const testUsername = 'dung+octopus4@101digital.io';
     const testPassword = 'Abc@123456';
@@ -13,18 +18,17 @@ describe('fetchAccessToken', () => {
     const testRefreshToken = 'testrefreshtoken';
 
     const mock = new MockAdapter(axios);
-    const data = { data: {access_token: testAccessToken, refresh_token: testRefreshToken} };
+    const data = { access_token: testAccessToken, refresh_token: testRefreshToken };
     mock.onPost(`${API.url}/token`).reply(200, data);
 
     fetchAccessToken(testUsername, testPassword).then(response => {
-      console.log(response);
       expect(response).toEqual(data); 
-    });
+    })
+
+    mock.reset();
   });
 
   it('throws an error if access token cannot be fetched', async () => {
-    const testUsername = 'testuser';
-    const testPassword = 'testpass';
     const testError = new Error('Failed to fetch access token');
 
     const axiosPostSpy = jest.spyOn(axios, 'post').mockRejectedValueOnce(testError);
@@ -36,5 +40,34 @@ describe('fetchAccessToken', () => {
     await expect(fetchAccessToken(testUsername, testPassword)).rejects.toThrow('Error fetching access token');
 
     axiosPostSpy.mockRestore();
+  });
+});
+
+describe('fetchOrgToken', () => {
+  const testAccessToken = 'testAccessToken';
+  const testOrgToken = 'testOrgToken';
+
+  it('returns the organization token if the API call succeeds', async () => {
+    const mock = new MockAdapter(axios);
+    mock.onGet(`${API.url}/membership-service/1.2.0/users/me`).reply(200, {
+      data: {
+        memberships: [{ token: testOrgToken }]
+      }
+    });
+
+    fetchOrgToken(testAccessToken).then((result) => {
+      expect(result).toEqual(testOrgToken);
+    })
+
+    mock.reset();
+  });
+
+  it('throws an error if the API call fails', async () => {
+    const mock = new MockAdapter(axios);
+    mock.onGet(`${API.url}/membership-service/1.2.0/users/me`).reply(500);
+
+    await expect(fetchOrgToken(testAccessToken)).rejects.toThrowError('Error fetching org token:');
+
+    mock.reset();
   });
 });
